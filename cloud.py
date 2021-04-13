661aa2ea1cd3199e005b715a133831d192278dcd
Jorgen Kvalvaag <jorgen.kvalvaag@nordicsemi.no>
2021-04-13 21:43:56 +0200

    init

---
+++ cloud.py
@@ -0,0 +1,64 @@
+import time
+import sys
+import re
+import json
+import argparse
+import uuid
+import os
+import threading
+import paho.mqtt.client as mqtt
+
+class NRFCloud(object):
+    def __init__(self, hostname, port, ca_certs, certfile, keyfile):
+        self.pl = None
+        self.client = mqtt.Client(client_id='nrfcloud-py')
+        self.client.tls_set(ca_certs=ca_certs, certfile=certfile, keyfile=keyfile)
+        self.client.on_connect = self.on_connect
+        self.client.on_message = self.on_message
+        self.client.on_subscribe = self.on_subscribe
+        self.client.on_publish = self.on_publish
+        self.client.connect(hostname, port=port)
+
+    def on_publish(self, client, userdata, mid):
+        print(f"published {mid}")
+
+    def start(self):
+        self.client.loop_forever()
+
+    def stop(self):
+        self.client.disconnect()
+        self.client.loop_stop()
+
+    def on_connect(self, client, userdata, flags, rc):
+        print("connected")
+        self.client.subscribe("#")
+        print("connected2")
+
+    def on_message(self, client, userdata, msg):
+        print(f"Message: {msg.topic}: {msg.payload}")
+
+    def on_disconnect(self, client, userdata, rc):
+        print("disconnected")
+
+    def on_subscribe(self, client, userdata, mid, granted_qos):
+        print(f"subscribe success {granted_qos} {mid}")
+
+def main(ca, cert, key):
+    import socket
+    print(socket.gethostname())
+    try:
+        nrfcloud = NRFCloud( 
+            "127.0.0.1", 18883, 
+            ca_certs=ca, 
+            certfile=cert, 
+            keyfile=key
+
+        )
+        nrfcloud.start()
+    except KeyboardInterrupt:
+        nrfcloud.stop()
+
+if __name__ == '__main__':
+	main("credentials/ca/cia-ca.pem",
+         "credentials/cert/client1-cert-signed-by-cia.pem",
+         "credentials/cert/client1-privkey.pem")
---
+++ credentials/manage.py
@@ -0,0 +1,132 @@
+import datetime
+import sys
+import os
+import argparse
+import ipaddress
+from cryptography import x509
+from cryptography.x509.oid import NameOID
+from cryptography.hazmat.primitives import serialization
+from cryptography.hazmat.primitives.asymmetric import rsa
+from cryptography.hazmat.primitives import hashes
+
+def generate_name(common_name):
+    return x509.Name([
+        x509.NameAttribute(NameOID.COUNTRY_NAME, "NO"),
+        x509.NameAttribute(NameOID.STATE_OR_PROVINCE_NAME, "Trd"),
+        x509.NameAttribute(NameOID.LOCALITY_NAME, "Trd"),
+        x509.NameAttribute(NameOID.ORGANIZATION_NAME, "Nordicsemi"),
+        x509.NameAttribute(NameOID.COMMON_NAME, common_name),
+    ])
+
+def generate_private_key():
+    return rsa.generate_private_key(
+        public_exponent=65537,
+        key_size=2048,
+    )
+
+def generate_san(dns_names, dns_ips):
+    alt_names = list()
+    if dns_names:
+        alt_names.extend([x509.DNSName(x) for x in dns_names])
+    if dns_ips:
+        alt_names.extend([x509.IPAddress(ipaddress.ip_address(x)) for x in dns_ips])
+
+    alt_names.append(x509.IPAddress(ipaddress.ip_address('127.0.0.1')))
+    alt_names.append(x509.DNSName('localhost'))
+    return x509.SubjectAlternativeName(alt_names)
+
+def load_cert(certfile):
+    print(f"Loaded certificate file: {certfile}")
+    with open(certfile, 'rb') as f:
+        return x509.load_pem_x509_certificate(f.read())
+
+def load_key(key):
+    with open(key, 'rb') as f:
+        return serialization.load_pem_private_key(
+            f.read(),
+            password=None
+        )
+
+def write_key(filename, key):
+    with open(filename, "wb") as f:
+        f.write(key.private_bytes(
+            encoding=serialization.Encoding.PEM,
+            format=serialization.PrivateFormat.TraditionalOpenSSL,
+            encryption_algorithm=serialization.NoEncryption(),
+        ))
+    print(f"Created: {filename}")
+
+def write_cert(filename, cert):
+    with open(filename, "wb") as f:
+        f.write(cert.public_bytes(serialization.Encoding.PEM))
+    print(f"Created: {filename}")
+
+def build_cert(subject, issuer, pubkey, privkey, san):
+    cert = x509.CertificateBuilder(
+    ).subject_name(
+        subject
+    ).issuer_name(
+        issuer
+    ).public_key(
+        pubkey.public_key()
+    ).serial_number(
+        x509.random_serial_number()
+    ).not_valid_before(
+        datetime.datetime.utcnow()
+    ).not_valid_after(
+        datetime.datetime.utcnow() + datetime.timedelta(days=3650)
+    )
+    if subject == issuer:
+        cert = cert.add_extension(
+            x509.BasicConstraints(ca=True, path_length=0), critical=False,
+        )
+    if san:
+        cert = cert.add_extension(san, False)
+    return cert.sign(privkey, hashes.SHA256())
+
+def generate_root_cert(name, san):
+    print(f"Generating self-signed {name}")
+    
+    key = generate_private_key()
+
+    issuer = subject = generate_name(f"root-{name}")
+    cert = build_cert(subject, issuer, key, key, san)
+
+    try:
+        os.mkdir("ca")
+    except:
+        pass
+
+    write_cert(f"ca/{name}-ca.pem", cert)
+    write_key(f"ca/{name}-privkey.pem", key)
+
+def generate_cert(root_name, cert_name, san):
+    print(f"Generating {cert_name} signed by {root_name}")
+    cert_key = generate_private_key()
+    try:
+        os.mkdir("cert")
+    except:
+        pass
+
+    root_key = load_key(f"ca/{root_name}-privkey.pem")
+    ca = load_cert(f"ca/{root_name}-ca.pem")
+    subject = generate_name(f"cert-{cert_name}")
+    cert = build_cert(subject, ca.issuer, cert_key, root_key, san)
+
+    write_cert(f"cert/{cert_name}-cert-signed-by-{root_name}.pem", cert)
+    write_key(f"cert/{cert_name}-privkey.pem", cert_key)
+
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--generate-root')
+    parser.add_argument('--generate-cert', nargs=2)
+    parser.add_argument('--dns-names', nargs='*')
+    parser.add_argument('--dns-ips', nargs='*')
+    args = parser.parse_args()
+    san = generate_san(args.dns_names, args.dns_ips)
+    if args.generate_root:
+        generate_root_cert(args.generate_root, san)
+    if args.generate_cert:
+        generate_cert(*args.generate_cert, san)
+
