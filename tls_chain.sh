4af1a7be281da9595e01ca33b172bfca80208a7a
Jorgen Kvalvaag <jorgen.kvalvaag@nordicsemi.no>
2021-04-15 14:59:41 +0200

    remove tls_chain.sh

--- tls_chain.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-pushd credentials >/dev/null 2>&1
-
-python3 manage.py --dns-names `hostname` --generate-root cia
-python3 manage.py --dns-names `hostname` --generate-cert cia client1
-python3 manage.py --dns-names `hostname` --generate-cert cia server1
